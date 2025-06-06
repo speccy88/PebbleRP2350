pebble-dev/pebble-firmware

fw: platform-specific ifdeffery to allow compile
PebbleOS was never prepared to be truly portable.
For now, let's survive as in nRF5 with more ifdeffery.
This needs a deep rework.

Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

src/fw/console/prompt_commands.c
@@ -997,7 +997,7 @@ void command_audit_delay_us(void) {
 void command_enter_stop(void) {
   dbgserial_putstr("Entering stop mode indefinitely ... reboot your board to get out!!");
   __disable_irq();
-#if !MICRO_FAMILY_NRF5
+#if !MICRO_FAMILY_NRF5 && !MICRO_FAMILY_SF32LB
   RTC_ITConfig(RTC_IT_WUT, DISABLE);
   RTC_WakeUpCmd(DISABLE);
 #endif

src/fw/kernel/core_dump_private.h
@@ -28,7 +28,7 @@
 // TODO: Do we have an equate for the total size of RAM somewhere else?
 #if PLATFORM_CALCULUS || PLATFORM_ROBERT
 #define COREDUMP_RAM_SIZE (384 * 1024)
-#elif PLATFORM_SILK || PLATFORM_ASTERIX
+#elif PLATFORM_SILK || PLATFORM_ASTERIX || PLATFORM_OBELIX
 #define COREDUMP_RAM_SIZE (256 * 1024)
 #elif PLATFORM_SNOWY || PLATFORM_SPALDING
 #define COREDUMP_RAM_SIZE (192 * 1024)
@@ -47,6 +47,8 @@
 #elif defined(MICRO_FAMILY_STM32F7)
 #define CORE_DUMP_MAX_IMAGES 3
 #elif defined(MICRO_FAMILY_NRF52840)
+#define CORE_DUMP_MAX_IMAGES 2
+#elif defined(MICRO_FAMILY_SF32LB)
 #define CORE_DUMP_MAX_IMAGES 2
 #else
 #error "Unsupported micro family"

speccy88/PebbleRP2350

src/fw/console/prompt_commands.c
@@ -1092,7 +1092,7 @@ void command_audit_delay_us(void) {
 void command_enter_stop(void) {
   dbgserial_putstr("Entering stop mode indefinitely ... reboot your board to get out!!");
   __disable_irq();
-#if !MICRO_FAMILY_NRF5
+#if !MICRO_FAMILY_NRF5 && !MICRO_FAMILY_SF32LB52
   RTC_ITConfig(RTC_IT_WUT, DISABLE);
   RTC_WakeUpCmd(DISABLE);
 #endif

src/fw/kernel/core_dump_private.h
@@ -28,7 +28,7 @@
 // TODO: Do we have an equate for the total size of RAM somewhere else?
 #if PLATFORM_CALCULUS || PLATFORM_ROBERT
 #define COREDUMP_RAM_SIZE (384 * 1024)
-#elif PLATFORM_SILK || PLATFORM_ASTERIX
+#elif PLATFORM_SILK || PLATFORM_ASTERIX || PLATFORM_OBELIX
 #define COREDUMP_RAM_SIZE (256 * 1024)
 #elif PLATFORM_SNOWY || PLATFORM_SPALDING
 #define COREDUMP_RAM_SIZE (192 * 1024)
@@ -47,6 +47,8 @@
 #elif defined(MICRO_FAMILY_STM32F7)
 #define CORE_DUMP_MAX_IMAGES 3
 #elif defined(MICRO_FAMILY_NRF52840)
+#define CORE_DUMP_MAX_IMAGES 2
+#elif defined(MICRO_FAMILY_SF32LB52)
 #define CORE_DUMP_MAX_IMAGES 2
 #else
 #error "Unsupported micro family"