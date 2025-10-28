a0082c6641b3acff6acb0f393e66275e6317a0f7
Gerard Marull-Paretas <gerard@teslabs.com>
2025-10-28 18:56:16 +0100

    fw/drivers/imu: add getafix IMU wrapper
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

---
+++ src/fw/drivers/imu/imu_getafix.c
@@ -0,0 +1,19 @@
+/* SPDX-FileCopyrightText: 2026 Core Devices LLC */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#include "drivers/imu.h"
+#include "drivers/imu/lis2dw12/lis2dw12.h"
+#include "drivers/imu/mmc5603nj/mmc5603nj.h"
+
+void imu_init(void) {
+  lis2dw12_init();
+  mmc5603nj_init();
+}
+
+void imu_power_up(void) {
+  lis2dw12_power_up();
+}
+
+void imu_power_down(void) {
+  lis2dw12_power_down();
+}
