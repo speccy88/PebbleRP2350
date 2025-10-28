7cec91ac7cfed0a23472f7b4c31d7ef64a9225ea
Gerard Marull-Paretas <gerard@teslabs.com>
2025-10-28 18:25:19 +0100

    fw/flash_region: add support for getafix
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- src/fw/flash_region/flash_region.h
+++ src/fw/flash_region/flash_region.h
@@ -30,7 +30,7 @@
 #include "flash_region_mt25q.h"
 #elif PLATFORM_SNOWY || PLATFORM_SPALDING
 #include "flash_region_s29vs.h"
-#elif PLATFORM_OBELIX
+#elif PLATFORM_OBELIX || PLATFORM_GETAFIX
 #include "flash_region_gd25q256e.h"
 #endif
 
