869a65fc932de735e549759984e2dd7efd1d3e0c
Gerard Marull-Paretas <gerard@teslabs.com>
2025-10-28 18:27:34 +0100

    fw/popups/bluetooth_pairing: tweaks for getafix
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- src/fw/popups/bluetooth_pairing_ui.c
+++ src/fw/popups/bluetooth_pairing_ui.c
@@ -117,7 +117,7 @@ static void prv_update_text_layer_with_translation(TextLayer *text_layer,
 }
 
 static void prv_update_prf_info_text_layers_text(BTPairingUIData *data) {
-#if PLATFORM_ROBERT || PLATFORM_CALCULUS || PLATFORM_OBELIX
+#if PLATFORM_ROBERT || PLATFORM_CALCULUS || PLATFORM_OBELIX || PLATFORM_GETAFIX
   const char *font_key_default = FONT_KEY_GOTHIC_28_BOLD;
   const char *font_key_japanese = FONT_KEY_MINCHO_24_PAIR;
 #else
@@ -262,7 +262,7 @@ static void prv_adjust_background_frame_for_state(BTPairingUIData *data) {
   switch (data->ui_state) {
     case BTPairingUIStateAwaitingUserConfirmation:
       alignment = GAlignTopLeft;
-#if PLATFORM_ROBERT || PLATFORM_CALCULUS || PLATFORM_OBELIX
+#if PLATFORM_ROBERT || PLATFORM_CALCULUS || PLATFORM_OBELIX || PLATFORM_GETAFIX
       x_offset = 39;
       y_offset = 85;
 #else
@@ -273,7 +273,7 @@ static void prv_adjust_background_frame_for_state(BTPairingUIData *data) {
       break;
     case BTPairingUIStateAwaitingResult:
       alignment = GAlignLeft;
-#if PLATFORM_ROBERT || PLATFORM_CALCULUS || PLATFORM_OBELIX
+#if PLATFORM_ROBERT || PLATFORM_CALCULUS || PLATFORM_OBELIX || PLATFORM_GETAFIX
       x_offset = 76;
       y_offset = 30;
 #else
@@ -285,7 +285,7 @@ static void prv_adjust_background_frame_for_state(BTPairingUIData *data) {
     case BTPairingUIStateFailed:
     case BTPairingUIStateSuccess:
       alignment = GAlignTop;
-#if PLATFORM_ROBERT || PLATFORM_CALCULUS || PLATFORM_OBELIX
+#if PLATFORM_ROBERT || PLATFORM_CALCULUS || PLATFORM_OBELIX || PLATFORM_GETAFIX
       x_offset = 0;
       y_offset = 59;
 #else
@@ -373,7 +373,7 @@ static void prv_window_load(Window *window) {
   const int32_t width_of_action_bar_with_padding = ACTION_BAR_WIDTH + PBL_IF_RECT_ELSE(2, -4);
   const int32_t width = window->layer.bounds.size.w - width_of_action_bar_with_padding;
   const int32_t x_offset = PBL_IF_RECT_ELSE(0, 22);
-#if PLATFORM_ROBERT || PLATFORM_CALCULUS || PLATFORM_OBELIX
+#if PLATFORM_ROBERT || PLATFORM_CALCULUS || PLATFORM_OBELIX || PLATFORM_GETAFIX
   const int32_t info_text_y_offset = 36;
 #else
   const int32_t info_text_y_offset = PBL_IF_RECT_ELSE(10, 12);
@@ -383,13 +383,13 @@ static void prv_window_load(Window *window) {
   kino_layer_init(kino_layer, &window->layer.bounds);
   layer_add_child(&window->layer, &kino_layer->layer);
 
-#if PLATFORM_ROBERT || PLATFORM_CALCULUS || PLATFORM_OBELIX
+#if PLATFORM_ROBERT || PLATFORM_CALCULUS || PLATFORM_OBELIX || PLATFORM_GETAFIX
   GRect pair_text_area = GRect(0, -2, width, 44);
 #else
   GRect pair_text_area = GRect(0, -2, width, 30);
 #endif
 
-#if PLATFORM_ROBERT || PLATFORM_CALCULUS || PLATFORM_OBELIX
+#if PLATFORM_ROBERT || PLATFORM_CALCULUS || PLATFORM_OBELIX || PLATFORM_GETAFIX
   layer_set_frame(&data->info_text_mask_layer, &GRect(x_offset, info_text_y_offset, width, 30));
 #else
   layer_set_frame(&data->info_text_mask_layer, &GRect(x_offset, info_text_y_offset, width, 26));
@@ -401,7 +401,7 @@ static void prv_window_load(Window *window) {
   text_layer_init_with_parameters(info_text_layer,
                                   &pair_text_area,
                                   data->info_text_layer_buffer,
-#if PLATFORM_ROBERT || PLATFORM_CALCULUS || PLATFORM_OBELIX
+#if PLATFORM_ROBERT || PLATFORM_CALCULUS || PLATFORM_OBELIX || PLATFORM_GETAFIX
                                   fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD),
 #else
                                   fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD),
@@ -423,7 +423,7 @@ static void prv_window_load(Window *window) {
 
   prv_add_prf_layers(pair_text_area, data);
 
-#if PLATFORM_ROBERT || PLATFORM_CALCULUS || PLATFORM_OBELIX
+#if PLATFORM_ROBERT || PLATFORM_CALCULUS || PLATFORM_OBELIX || PLATFORM_GETAFIX
   const int16_t y_offset = 55;
 #else
   const int16_t y_offset = PBL_IF_RECT_ELSE(0, 2);
