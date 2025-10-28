c798fddf9b4872b122cf6212890e6fae02a91893
Gerard Marull-Paretas <gerard@teslabs.com>
2025-10-28 15:06:43 +0100

    fw/drivers/vibe: introduce vibe_calibrate()
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- src/fw/drivers/vibe.h
+++ src/fw/drivers/vibe.h
@@ -19,6 +19,8 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#include "system/status_codes.h"
+
 // motor full power
 #define VIBE_STRENGTH_MAX 100
 // motor full reverse
@@ -33,3 +35,12 @@ void vibe_set_strength(int8_t strength);
 
 //! Return the strength that should be used for braking the motor to a stop.
 int8_t vibe_get_braking_strength(void);
+
+//! Calibrate the vibe motor driver for optimal performance.
+//!
+//! If vibration motor has NV memory, calibration data will be stored there on
+//! a successful calibration.
+//!
+//! @retval S_SUCCESS indicating success or failure of calibration.
+//! @retval error Code indicating failure of calibration.
+status_t vibe_calibrate(void);
