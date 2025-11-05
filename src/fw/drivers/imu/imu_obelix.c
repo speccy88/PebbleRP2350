8a5799cecec1f9d4fa7f123a7b5b057112613c3f
Gerard Marull-Paretas <gerard@teslabs.com>
2025-11-05 13:12:00 +0100

    fw/drivers/imu/obelix: add support for LIS2DW12
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- src/fw/drivers/imu/imu_obelix.c
+++ src/fw/drivers/imu/imu_obelix.c
@@ -16,17 +16,30 @@
 
 #include "drivers/imu.h"
 #include "drivers/imu/lsm6dso/lsm6dso.h"
+#include "drivers/imu/lis2dw12/lis2dw12.h"
 #include "drivers/imu/mmc5603nj/mmc5603nj.h"
 
 void imu_init(void) {
+#ifdef IMU_USE_LIS2DW12
+  lis2dw12_init();
+#else
   lsm6dso_init();
+#endif
   mmc5603nj_init();
 }
 
 void imu_power_up(void) {
+#ifdef IMU_USE_LIS2DW12
+  lis2dw12_power_up();
+#else
   lsm6dso_power_up();
+#endif
 }
 
 void imu_power_down(void) {
+#ifdef IMU_USE_LIS2DW12
+  lis2dw12_power_down();
+#else
   lsm6dso_power_down();
+#endif
 }
