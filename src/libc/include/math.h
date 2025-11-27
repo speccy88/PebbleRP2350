69a9bbff27eb1aa4092598ae12963e0c66fb81d5
Gerard Marull-Paretas <gerard@teslabs.com>
2025-11-27 13:15:56 +0100

    libc: extend with some double/float functions
    These are required by GH3X2X binary blobs
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- src/libc/include/math.h
+++ src/libc/include/math.h
@@ -9,23 +9,42 @@
 #endif
 
 #define INFINITY   ((double)(_HUGE_ENUF * _HUGE_ENUF))
+#define NAN        ((double)(INFINITY * 0.0F))
 
 #define INFINITY_F ((float)INFINITY)
 #define NAN_F      ((float)(INFINITY * 0.0F))
 
 #define M_E        2.7182818284590452353602874713526625
+#define M_LN10     2.3025850929940456840179914546843642
 #define M_PI       3.1415926535897932384626433832795029
+#define M_PI_2     1.5707963267948966192313216916397514
 
 typedef float float_t;
 
 typedef double double_t;
 
+#define isinf(x)   (((x) == INFINITY) || ((x) == -INFINITY))
 #define isinff(x)  (((x) == INFINITY_F) || ((x) == -INFINITY_F))
+#define isnan(x)   ((x) != (x))
 #define isnanf(x)  ((x) != (x))
+
+double asin(double x);
+
+double atan(double x);
+
+double ceil(double x);
 
 float ceilf(float x);
 
+double cos(double x);
+
+double copysign(double x, double y);
+
+double exp(double x);
+
 float expf(float x);
+
+double fabs(double x);
 
 float fabsf(float x);
 
@@ -35,10 +54,24 @@ float fmaxf(float x, float y);
 
 float fminf(float x, float y);
 
+double fmod(double x, double div);
+
 float logf(float x);
+
+float log10f(float x);
+
+double modf(double x, double *iptr);
 
 float modff(float x, float *iptr);
 
 double round(double d);
 
 float roundf(float x);
+
+double sin(double x);
+
+double sqrt(double x);
+
+float tanhf(float x);
+
+#define signbit(x)           __builtin_signbit(x)
