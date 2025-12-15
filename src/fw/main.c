a7932f4fd00f71e5b3c27f056a91ec1a0123f3e7
Gerard Marull-Paretas <gerard@teslabs.com>
2025-12-15 23:35:45 +0100

    fw/main: invalidate slot0/1 on PRF
    Invalidate slot0/1 when booting PRF, so we force main firmware
    re-install.
    
    Fixes FIRM-1008
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- src/fw/main.c
+++ src/fw/main.c
@@ -66,6 +66,7 @@
 #include "kernel/kernel_applib_state.h"
 #include "kernel/util/delay.h"
 #include "util/mbuf.h"
+#include "system/firmware_storage.h"
 #include "system/version.h"
 
 #include "kernel/event_loop.h"
@@ -456,6 +457,12 @@ static NOINLINE void prv_main_task_init(void) {
 
   // Do this early before things can screw ith it.
   check_prf_update();
+
+#if CAPABILITY_HAS_PBLBOOT && defined(RECOVERY_FW) && !defined(MANUFACTURING_FW)
+  // Invalidate slot0/1 when booting PRF, so we force main firmware re-install
+  firmware_storage_invalidate_firmware_slot(0);
+  firmware_storage_invalidate_firmware_slot(1);
+#endif
 
   // When there are new system resources waiting to be installed, this call
   // will actually install them:
