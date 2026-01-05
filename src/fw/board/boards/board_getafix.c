457a644e556c0df3aa3149226846a1def9650d66
Gerard Marull-Paretas <gerard@teslabs.com>
2026-01-05 15:57:45 +0100

    fw/boards/getafix: remove unnecessary wake-up sources
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- src/fw/board/boards/board_getafix.c
+++ src/fw/board/boards/board_getafix.c
@@ -599,12 +599,6 @@ void board_init(void) {
 
   mic_init(MIC);
 
-  /* Enable user buttons as AON wakeup source */
-  HAL_HPAON_EnableWakeupSrc(HPAON_WAKEUP_SRC_PIN10, AON_PIN_MODE_HIGH);
-  HAL_HPAON_EnableWakeupSrc(HPAON_WAKEUP_SRC_PIN11, AON_PIN_MODE_LOW);
-  HAL_HPAON_EnableWakeupSrc(HPAON_WAKEUP_SRC_PIN12, AON_PIN_MODE_LOW);
-  HAL_HPAON_EnableWakeupSrc(HPAON_WAKEUP_SRC_PIN13, AON_PIN_MODE_LOW);
-
 #ifdef RECOVERY_FW
   // FIXME(GETAFIX): Remove once stable
   stop_mode_disable(InhibitorMain);
