cf2c82c24d5f7ba18e7eb67c2200813d77a8bd33
Gerard Marull-Paretas <gerard@teslabs.com>
2026-01-09 16:28:18 +0100

    fw/board/obelix: fix accelerometer axes config
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- src/fw/board/boards/board_obelix.h
+++ src/fw/board/boards/board_obelix.h
@@ -55,8 +55,8 @@ static const BoardConfigAccel BOARD_CONFIG_ACCEL = {
     .axes_offsets[AXIS_X] = 1,
     .axes_offsets[AXIS_Y] = 0,
     .axes_offsets[AXIS_Z] = 2,
-    .axes_inverts[AXIS_X] = false,
-    .axes_inverts[AXIS_Y] = true,
+    .axes_inverts[AXIS_X] = true,
+    .axes_inverts[AXIS_Y] = false,
     .axes_inverts[AXIS_Z] = true,
 #endif
     // TODO(OBELIX): Needs calibration
