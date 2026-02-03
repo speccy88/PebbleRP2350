2e0f10921b119b56c8b5f0788aa7f7170c0029d9
Gerard Marull-Paretas <gerard@teslabs.com>
2026-02-03 12:23:52 +0100

    fw/drivers/pressure: add stub driver for BMP390
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

---
+++ src/fw/drivers/pressure.h
@@ -0,0 +1,9 @@
+/* SPDX-FileCopyrightText: 2026 Core Devices LLC */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#pragma once
+
+#include <stdbool.h>
+
+//! Initialize the pressure sensor driver. Call this once at startup.
+void pressure_init(void);
