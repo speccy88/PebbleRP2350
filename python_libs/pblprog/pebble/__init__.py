dfc8e7955ef9f07e44f15883f1f1cdca2f2fa63e
Gerard Marull-Paretas <gerard@teslabs.com>
2026-02-12 14:34:17 +0100

    python_libs: fix setuptools issues
    Removed the __import__('pkg_resources').declare_namespace(__name__)
    call from each. The pebble namespace package now uses Python's
    implicit namespace package mechanism (PEP 420), which doesn't require
    setuptools/pkg_resources and works with Python 3.3+.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- python_libs/pblprog/pebble/__init__.py
+++ python_libs/pblprog/pebble/__init__.py
@@ -1,4 +1,3 @@
 # SPDX-FileCopyrightText: 2024 Google LLC
 # SPDX-License-Identifier: Apache-2.0
 
-__import__('pkg_resources').declare_namespace(__name__)
--- python_libs/pebble-loghash/pebble/__init__.py
+++ python_libs/pebble-loghash/pebble/__init__.py
@@ -1,7 +1,3 @@
 # SPDX-FileCopyrightText: 2025 Google LLC
 # SPDX-License-Identifier: Apache-2.0
 
-"""
-Namespace module
-"""
-__import__('pkg_resources').declare_namespace(__name__)
