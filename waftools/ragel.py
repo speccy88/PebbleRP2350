1969faf4d49716bdfaaadf6042e4c27d1dc217cf
Gerard Marull-Paretas <gerard@teslabs.com>
2026-02-24 20:30:21 +0100

    waftools: remove dead scripts
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- waftools/ragel.py
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: 2024 Google LLC
-# SPDX-License-Identifier: Apache-2.0
-
-
-def ragel(task):
-    task.exec_command(
-        'ragel -o "%s" -C "%s"' % (task.outputs[0].abspath(), task.inputs[0].abspath())
-    )
