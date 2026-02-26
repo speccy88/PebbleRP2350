fe2511d7394852644cbc58dd1b7adaafbad4ac3f
Gerard Marull-Paretas <gerard@teslabs.com>
2026-02-26 13:40:35 +0100

    third_party/nimble/transport/hci_sf32lb52: handle hci_h4_sm_rx error
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- third_party/nimble/transport/hci_sf32lb52.c
+++ third_party/nimble/transport/hci_sf32lb52.c
@@ -230,6 +230,10 @@ static void prv_hci_task_main(void *unused) {
           int consumed_bytes;
 
           consumed_bytes = hci_h4_sm_rx(&s_hci_h4sm, pbuf, len);
+          if (consumed_bytes <= 0) {
+            PBL_LOG_D_ERR(LOG_DOMAIN_BT_STACK, "hci_h4_sm_rx rc=%d", consumed_bytes);
+            break;
+          }
           len -= consumed_bytes;
           pbuf += consumed_bytes;
         }
