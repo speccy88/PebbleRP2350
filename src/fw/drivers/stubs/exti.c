1de599d719fe47ca639ff1d24d4de710df1f0ae7
Gerard Marull-Paretas <gerard@teslabs.com>
2026-04-01 23:42:51 +0200

    treewide: add more missing SPDX headers
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- src/fw/drivers/stubs/exti.c
+++ src/fw/drivers/stubs/exti.c
@@ -1,3 +1,6 @@
+/* SPDX-FileCopyrightText: 2025 Core Devices LLC */
+/* SPDX-License-Identifier: Apache-2.0 */
+
 #include "drivers/exti.h"
 
 void exti_configure_pin(ExtiConfig cfg, ExtiTrigger trigger, ExtiHandlerCallback cb) {
