f0833b15035556d2d281f977a61282daa782bee8
Gerard Marull-Paretas <gerard@teslabs.com>
2026-04-01 19:00:00 +0200

    third_party/cmsis_core: add glue header
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

---
+++ third_party/cmsis_core/cmsis_core.h
@@ -0,0 +1,17 @@
+/* SPDX-FileCopyrightText: 2026 Core Devices LLC */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#pragma once
+
+#if defined(MICRO_FAMILY_STM32F4)
+#include <stm32f4xx.h>
+#elif defined(MICRO_FAMILY_NRF52840)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmissing-declarations"
+#include <nrf52840.h>
+#pragma GCC diagnostic pop
+#elif defined(MICRO_FAMILY_SF32LB52)
+#include <bf0_hal.h>
+#else
+#error "Unknown or missing MICRO_FAMILY_* define"
+#endif
