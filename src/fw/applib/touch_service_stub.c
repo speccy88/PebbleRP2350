3a3e5f475b06607a3a792dc82db9fd8f4e1ecbae
Joshua Jun <lets@throw.rocks>
2026-04-20 15:10:25 +0200

    fw/applib: add touch_service stub for boards without touch
    Signed-off-by: Joshua Jun <lets@throw.rocks>

---
+++ src/fw/applib/touch_service_stub.c
@@ -0,0 +1,12 @@
+/* SPDX-FileCopyrightText: 2026 Core Devices LLC */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#include "touch_service.h"
+
+void touch_service_subscribe(TouchServiceHandler handler, void *context) {
+  (void)handler;
+  (void)context;
+}
+
+void touch_service_unsubscribe(void) {
+}
